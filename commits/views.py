262ed0609f5c5e3124b0a39421fb0dec672a67a0
Neil Yao <yaozhifeng@gmail.com>
2011-11-13 14:49:46 +0800

    graph for project and person

--- commits/views.py
+++ commits/views.py
@@ -6,6 +6,7 @@ from django.db.models import Sum, Count
 import datetime
 from models import *
 
+#home view
 def home(request):
     #project list
     projects = Repository.objects.values('id', 'name', 'desc')
@@ -59,8 +60,14 @@ def home(request):
                 context_instance = RequestContext(request)
             )
 
+#project view    
 def project(request, project_id):
     project = get_object_or_404(Repository, pk=project_id)
+
+    today = datetime.date.today()
+    lastmonth_day = today - datetime.timedelta(days=30)
+
+    #recent commits
     commits = project.commits.values(
             'revision',
             'comment',
@@ -68,15 +75,34 @@ def project(request, project_id):
             'author__account',
             'author__display').order_by('-time')[:10]
 
+    #participants
     coders = Author.objects.filter(commits__repository=project)
     coders = coders.annotate(author_commits=Count('commits'))
     coders = coders.order_by('-author_commits')
     
+    #graph of participants contribution
+    #data for generating the graph
+    select_data = {"date": """strftime('%%Y-%%m-%%d', time)"""}
+    graph = CommitLog.objects.filter(time__range=(lastmonth_day, today)).filter(repository=project)
+    graph = graph.extra(select=select_data).values('date').annotate(commit_count=Count('id'))
+
+    stats = {}
+    for coder in coders:
+        g = graph.filter(author=coder)
+        line = '['
+        for day in g:
+            line = line + '["%s", %d],' % (day['date'], day['commit_count'])
+        line = line + ']'
+
+        if line != '[]':
+            stats[coder.display if len(coder.display)>0 else coder.account] = line
+
     return render_to_response('project.html',
             {
                 'project': project,
                 'commits': commits,
                 'coders': coders,
+                'graph': stats,
             },
             context_instance = RequestContext(request)
         )
@@ -86,23 +112,47 @@ def person(request, person_id):
     #pdb.set_trace();
     author = get_object_or_404(Author, pk=person_id)
     
+    #recent commits
     commits = CommitLog.objects.filter(author=author)
     commits = commits.values('repository__name', 'time', 'comment')
     commits = commits.order_by('-time')[0:10]
 
+    #personal information
     coder = {}
     coder['account'] = author.account
     coder['display'] = author.display
     coder['commits'] = CommitLog.objects.filter(author__id=person_id).count()
     coder['recent_commits'] = 0
 
-    projects = Repository.objects.values('name').filter(commits__author=author).annotate(commits_count=Count('commits'))
+    #participated projects
+    projects = Repository.objects.values('id', 'name').filter(commits__author=author).annotate(commits_count=Count('commits'))
+
+    today = datetime.date.today()
+    lastmonth_day = today - datetime.timedelta(days=30)
+
+    #contribution graph for each project
+    select_data = {"date": """strftime('%%Y-%%m-%%d', time)"""}
+    graph = CommitLog.objects.filter(time__range=(lastmonth_day, today)).filter(author=author)
+    graph = graph.extra(select=select_data).values('date').annotate(commit_count=Count('id'))
+
+    stats = {}
+    for project in projects:
+        g = graph.filter(repository=project['id'])
+        line = '['
+        for day in g:
+            line = line + '["%s", %d],' % (day['date'], day['commit_count'])
+        line = line + ']'
+ 
+        stats[project['name']] = line
+
+       
 
     return render_to_response('person.html',
             {
                 'coder': coder,
                 'commits': commits,
                 'projects': projects,
+                'graph': stats,
             },
             context_instance = RequestContext(request)
         )
