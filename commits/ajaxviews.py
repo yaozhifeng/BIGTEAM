ed9d89dfcd0275f763264ea5babd0bb970388b13
Neil Yao <yaozhifeng@gmail.com>
2011-11-17 22:06:14 +0800

    change home page graphs to High charts

--- commits/ajaxviews.py
+++ commits/ajaxviews.py
@@ -13,18 +13,11 @@ def summary(request, year, month):
     if not request.is_ajax():
         return HttpResponse(status=400)
 
-    #data for generating the graph
-    select_data = {"date": """strftime('%%Y-%%m-%%d', time)"""}
+    select_data = {"year": """strftime('%%Y', time)""", "month": """strftime('%%m', time)""", "day": """strftime('%%d', time)"""}
     graph = CommitLog.objects.filter(time__year=year, time__month=month)
-    graph = graph.extra(select=select_data).values('date').annotate(commit_count=Count('id'))
-    #data = json.dumps(list(graph))
-    data = '['
-    for day in graph:
-        data = data + '["%s", %d],' % (day['date'], day['commit_count'])
-    data = data + ']'
+    graph = graph.extra(select=select_data).values('year', 'month', 'day').annotate(commit_count=Count('id'))
 
-    #pdb.set_trace()
-    return HttpResponse(json.dumps(data), 'application/json')
+    return HttpResponse(json.dumps(list(graph)), 'application/json')
 
 #ajax view for daily commits statistics
 #project id is optional
