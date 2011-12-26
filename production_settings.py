4373bc9aea5a7d68ed33e67d49ec8758d7eac466
Neil Yao <yaozhifeng@gmail.com>
2011-12-26 15:00:42 +0800

    refine production settings

--- production_settings.py
+++ production_settings.py
@@ -63,12 +63,12 @@ STATIC_ROOT = os.path.join(SETTINGS_PATH, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/bigteam/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/bigteam/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
