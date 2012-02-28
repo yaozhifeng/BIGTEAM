783d94e91d66cd2cd1a32a73f2959834a37305df
Neil Yao <yaozhifeng@gmail.com>
2012-02-28 21:31:11 +0800

    add markdown README file

---
+++ README.md
@@ -0,0 +1,30 @@
+BIG TEAM
+========
+
+Bigteam is a simple web application to measure team productivity. It checks your subversion repositories and give you an overview of how your team members hade been doing. It include 4 simple views.
+
+Home view
+---------
+
+Home view shows the overview of all projects and team member performance.
+
+Project View
+------------
+
+Project view shows member performance within a project.
+
+Personal View
+-------------
+
+Personal view shows detail of each individuel, including contribution to each project, all commits, and detailed commit log.
+
+Statistics Period
+-----------------
+
+Big Team gives statistics by month. You can roll over different period within the whole history of each view.
+
+Devlopment
+----------
+
+Big team is a simple Python Django application. Feel free to fork.
+
---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import *
+from fabric.contrib.files import exists
+
+env.hosts = ['chenxia@172.16.16.38']
+env.password = 'Ie4cgqrx'
+site_dir = '/var/sites/bigteam'
+
+@task
+def migrate():
+    with cd(site_dir):
+        sudo('python manage.py migrate')
+
+@task
+def deploy():
+    with cd(site_dir):
+        sudo('git pull')
+        sudo('touch django.wsgi')
+
+@task
+def backup():
+    with cd(site_dir):
+        if not exists('backup'):
+            sudo('mkdir backup')
+        if exists('backup/repository.json'):
+            sudo('rm backup/repository.json')
+        sudo('python manage.py dumpdata commits.repository --indent 4 >> \
+                ./backup/repository.json')
+        get('backup/repository.json', './backup/repository.json')
+        if exists('backup/author.json'):
+            sudo('rm backup/author.json')
+        sudo('python manage.py dumpdata commits.author --indent 4 >> \
+                ./backup/author.json')
+        get('backup/author.json', './backup/author.json')
+
