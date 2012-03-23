a783818607c73b4d53a50838e54d3868cb269c04
Neil Yao <yaozhifeng@gmail.com>
2012-03-23 13:28:25 +0800

    use local_settings to simplify stuff

---
+++ local_settings.sample.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bigteam',                      # Or path to database file if using sqlite3.
+        'USER': 'bigteam',                      # Not used with sqlite3.
+        'PASSWORD': 'dbFJeJBvYYMbhLSr',       # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
